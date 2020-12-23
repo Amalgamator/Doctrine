e242faf2eda13855ac4f1a1e146b2fa718c49067
Amalgamator <gunther.leeenaert@gmail.com>
2020-12-23 16:06:31 +0100

    Add docstr to admin class

--- Features/Main/admin.py
+++ Features/Main/admin.py
@@ -3,14 +3,16 @@ from discord.ext import commands
 
 
 class admin(commands.Cog):
+    """Administrative commands, listeners, and optional states."""
 
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    @commands.has_role(786679773866098708)
-    @commands.guild_only()
+    @commands.command()  # Listens for msgs with command prefix
+    @commands.has_role(786679773866098708)  # message author has dev role
+    @commands.guild_only()  # No private messages
     async def hello(self, ctx):
+        """Responds with a hello message if a dev says hello."""
         await ctx.send('Hello developer {0.display_name}.'.format(ctx.author))
 
 
