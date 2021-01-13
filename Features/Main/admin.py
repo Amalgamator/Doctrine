d0534db2efa4a5488ce07b11de69d7a7c4df4947
Amalgamator <gunther.leeenaert@gmail.com>
2021-01-13 12:04:38 +0100

    updated dev id

--- Features/Main/admin.py
+++ Features/Main/admin.py
@@ -19,14 +19,14 @@ class Admin(commands.Cog):
         self.bot = bot
 
     @commands.command()  # Listens for msgs with command prefix
-    @commands.has_role(786679773866098708)  # message author has dev role
+    @commands.has_role(797016628629340200)  # message author has dev role
     @commands.guild_only()  # No private messages
     async def hello(self, ctx):
         """Responds with a hello message if a dev says hello."""
         await ctx.send('Hello developer {0.display_name}.'.format(ctx.author))
 
     @commands.command()  # Listens for msgs with command prefix
-    @commands.has_role(786679773866098708)  # message author has dev role
+    @commands.has_role(797016628629340200)  # message author has dev role
     @commands.guild_only()  # No private messages
     async def reload(self, ctx):
         """Reloads all extensions."""
