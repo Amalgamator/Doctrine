6dfd57ebd6ae959cb511466ae1cd3f8ba4c65920
Amalgamator <gunther.leeenaert@gmail.com>
2021-01-13 21:53:45 +0100

    tr

--- trial.py
+++ trial.py
@@ -1,7 +1,43 @@
 import os
+'''
+    For the given path, get the List of all files in the directory tree
+'''
+def getListOfFiles(dirName):
+    # create a list of file and sub directories
+    # names in the given directory
+    listOfFile = os.listdir(dirName)
+    allFiles = list()
+    # Iterate over all the entries
+    for entry in listOfFile:
+        # Create full path
+        fullPath = os.path.join(dirName, entry)
+        # If entry is a directory then get the list of files in this directory
+        if os.path.isdir(fullPath):
+            allFiles = allFiles + getListOfFiles(fullPath)
+        else:
+            allFiles.append(fullPath)
 
-for dirpath, dirs, files in os.walk('/home/threevr/Doctrine/Features'):
-  for filename in files:
-    fname = os.path.join(dirpath,filename)
-    if fname.endswith('.py'):
-      print(fname)
+    return allFiles
+def main():
+    dirName = '/home/threevr/Doctrine/Features';
+
+    # Get the list of all files in directory tree at given path
+    listOfFiles = getListOfFiles(dirName)
+
+    # Print the files
+    for elem in listOfFiles:
+        print(elem)
+    print ("****************")
+
+    # Get the list of all files in directory tree at given path
+    listOfFiles = list()
+    for (dirpath, dirnames, filenames) in os.walk(dirName):
+        listOfFiles += [os.path.join(dirpath, file) for file in filenames]
+
+
+    # Print the files
+    for elem in listOfFiles:
+        print(elem)
+
+    if __name__ == '__main__':
+        main()
